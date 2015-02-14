88bd66a12af81c3e36182cbfa7e9047eb8a49af6
Kul Subedi <subedi.kulprasad@gmail.com>
2015-02-13 20:07:47 -0600

    Added

--- algorithm_practice/binary_tree.py
+++ algorithm_practice/binary_tree.py
@@ -2,22 +2,43 @@
 class BinaryNode:
     def __init__(self,value = None):
         "Create binary Node"    
-	self.value = None
+	self.value = value
 	self.left = None
 	self.right = None
 	def add(self,val):
 		'''Add a new node to the tree containing '''
 		if val<=self.value:
 			if self.left:
+				self.left.add(val)
+			else:
+				self.left = BinaryNode(val)
+		else:
+			if self.right:
+				self.right.add(val)
+			else:
+				self.right = BinaryNode(val)
+
 
 
 class BinaryTree:
     def __init__(self):
 	'''Create empty binary tree'''	    
-	    self.root = None
-	def add(self, value):
+        self.root = None
+    def add(self, value):
 		'''Insert value inot proper location in Binary Tree'''
 		if self.root is None:
 			self.root = BinaryNode(value)
 		else:
 		    self.root.add(value)
+    def contains(self, target):
+		'''check'''
+		node = self.root
+		while node:
+			if target == node.value:
+				return True
+			if target < node.value:
+				node = node.left
+			else:
+				node = node.right
+		return False
+
---
+++ algorithm_practice/test_binarytree.py
@@ -0,0 +1,12 @@
+from binary_tree import *
+
+
+def main():
+	bt = BinaryTree()
+	bt.add(5)
+	print bt.contains(5)
+
+
+if __name__=='__main__':
+	main()
+
